5aa72ba2c1a5e969d4d673eb2acb306f23a616ea
Nathalie Flower <nathalie@bango.com>
2019-12-30 12:05:01 +0000

    Added Reverse and Add project for TeamCity Testing

--- ReverseAndAdd_6.8.19/Program.cs
+++ ReverseAndAdd_6.8.19/Program.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ReverseAndAdd_6._8._19
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int sumTotal;
             int sumTotalReverse;
 
             Console.WriteLine("Enter a number: ");
-            var inputNumber = int.Parse(Console.ReadLine());
+            var inputNumber = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
             do
             {
---
+++ UnitTests/ReverseAndAddTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml;
+using Xunit;
+
+
+namespace UnitTests
+{
+    public class ReverseAndAddTests
+    {
+        [Fact]
+        public void ReverseNumberReturnsReversedNumber()
+        {
+            var input = 382687;
+
+            var result = ReverseAndAdd_6._8._19.Program.ReverseNumber(input);
+
+            Assert.Equal(786283, result);
+        }
+    }
+}
+
